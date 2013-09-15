e5aa5e28f425d2b2ab182d4f6a15bd21deb904b2
Paul Groves <github@grov.es>
2013-09-15 20:39:22 +0100

    Adding Railroady doc diagrams

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,13 +2,13 @@ require 'active_support/inflector'
 
 class Collection < ActiveRecord::Base
 
+  include Sluggable
   has_many :things, dependent: :destroy
   has_many :properties, dependent: :destroy
   accepts_nested_attributes_for :properties, allow_destroy: true
   validates_presence_of :name
   validates_uniqueness_of :name
   before_validation :pluralize_name
-  include Sluggable
 
   def pluralize_name
     self.name = name.pluralize
